dd7d8ff3060f4e8df94727ed6e940e85b12c67e8
Michal <szpunar98@gmail.com>
2020-02-06 12:14:57 +0100

    final version

--- extracting_data_script/results_to_latex.py
+++ extracting_data_script/results_to_latex.py
@@ -1,21 +1,33 @@
 import re
 import statistics
-from os import listdir
-from os.path import isfile, join
-from decimal import Decimal
 
 
+'''
 DIM_30_MOD = 'all_results\\dim_30_mod\\'
 DIM_30_REF = 'all_results\\dim_30_ref\\'
 DIMS_MOD = 'all_results\\dims_mod\\'
 DIMS_REF = 'all_results\\dims_ref\\'
+'''
 
-TERMINATIONS = ['maxfevals', 'condition', 'tolfun']
+PATH_MOD = 'home\\wojciech\\Desktop\\all_data\\result_out_mod\\'
+PATH_REF = 'home\\wojciech\\Desktop\\all_data\\result_out_ref\\'
 
+TERMINATIONS = ['maxfevals', 'condition', 'tolfun', 'tolx']
+
+MIN_VALUES = []
+temp = -1400
+for i in range(1,29):
+    MIN_VALUES.append(temp)
+    temp += 100
+    if temp == 0:
+        temp = 100
+
+filenames = []
 
 def process_data(filename, iteration_arr, fvalue_arr, termination_arr, solution_arr):
     try:
         rfile = open(filename, "r")
+        filenames.append(filename)
     except FileNotFoundError:
         return
     content = rfile.readlines()
@@ -66,7 +78,6 @@ def process_data(filename, iteration_arr, fvalue_arr, termination_arr, solution_
 
 
 
-
 dim = 5
 dim5_fvalue_arr = []
 dim5_termination_arr = []
@@ -80,7 +91,7 @@ for i in range(1, 29):  # for every CEC test function 1-28 (i)
     cec_sol_arr = []
 
     for k in range(51):  # for every point
-        filename = DIMS_REF + str(k) + "_dim_" + str(dim) + "_cec_" + str(i) + "_tourcand_2_data"
+        filename = PATH_REF + str(k) + "_dim_" + str(dim) + "_cec_" + str(i) + "_tourcand_2_data"
         process_data(filename, iteration_arr=cec_iter_arr, fvalue_arr=cec_fvalue_arr, termination_arr=cec_term_arr,
                      solution_arr=cec_sol_arr)
 
@@ -88,11 +99,6 @@ for i in range(1, 29):  # for every CEC test function 1-28 (i)
     dim5_termination_arr.append(cec_term_arr)
     dim5_solution_arr.append(cec_sol_arr)
     dim5_iteration_arr.append(cec_iter_arr)
-
-# print(dim5_iteration_arr)
-# print(dim5_solution_arr)
-# print(dim5_termination_arr)
-# print(dim5_fvalue_arr)
 
 
 
@@ -109,7 +115,7 @@ for i in range(1, 29):  # for every CEC test function 1-28 (i)
     cec_sol_arr = []
 
     for k in range(51):  # for every point
-        filename = DIMS_REF + str(k) + "_dim_" + str(dim) + "_cec_" + str(i) + "_tourcand_2_data"
+        filename = PATH_REF + str(k) + "_dim_" + str(dim) + "_cec_" + str(i) + "_tourcand_2_data"
         process_data(filename, iteration_arr=cec_iter_arr, fvalue_arr=cec_fvalue_arr, termination_arr=cec_term_arr,
                      solution_arr=cec_sol_arr)
 
@@ -133,7 +139,7 @@ for i in range(1, 29):  # for every CEC test function 1-28 (i)
     cec_sol_arr = []
 
     for k in range(51):  # for every point
-        filename = DIMS_REF + str(k) + "_dim_" + str(dim) + "_cec_" + str(i) + "_tourcand_2_data"
+        filename = PATH_REF + str(k) + "_dim_" + str(dim) + "_cec_" + str(i) + "_tourcand_2_data"
         process_data(filename, iteration_arr=cec_iter_arr, fvalue_arr=cec_fvalue_arr, termination_arr=cec_term_arr,
                      solution_arr=cec_sol_arr)
 
@@ -156,7 +162,7 @@ for i in range(1, 29):  # for every CEC test function 1-28 (i)
     cec_sol_arr = []
 
     for k in range(51):  # for every point
-        filename = DIM_30_REF + str(k) + "_dim_" + str(dim) + "_cec_" + str(i) + "_tourcand_2_data"
+        filename = PATH_REF + str(k) + "_dim_" + str(dim) + "_cec_" + str(i) + "_tourcand_2_data"
         process_data(filename, iteration_arr=cec_iter_arr, fvalue_arr=cec_fvalue_arr, termination_arr=cec_term_arr,
                      solution_arr=cec_sol_arr)
 
@@ -181,13 +187,13 @@ for i in range(1, 29):  # for every CEC test function 1-28 (i)
     cec_iter_arr = []
     cec_term_arr = []
     cec_sol_arr = []
-    for j in range(2, 7):  # for every size of tournament
+    for j in range(1, 9):  # for every size of tournament
         tour_fvalue_arr = []
         tour_iter_arr = []
         tour_term_arr = []
         tour_sol_arr = []
         for k in range(51):  # for every point
-            filename = DIMS_MOD +str(k) + "_dim_" + str(dim) + "_cec_" + str(i) + "_tourcand_" + str(j) + "_data"
+            filename = PATH_MOD +str(k) + "_dim_" + str(dim) + "_cec_" + str(i) + "_tourcand_" + str(j) + "_data"
             process_data(filename, iteration_arr=tour_iter_arr, fvalue_arr=tour_fvalue_arr, termination_arr=tour_term_arr,
                          solution_arr=tour_sol_arr)
         cec_fvalue_arr.append(tour_fvalue_arr)
@@ -211,13 +217,13 @@ for i in range(1, 29):  # for every CEC test function 1-28 (i)
     cec_iter_arr = []
     cec_term_arr = []
     cec_sol_arr = []
-    for j in range(2, 7):  # for every size of tournament
+    for j in range(1, 9):  # for every size of tournament
         tour_fvalue_arr = []
         tour_iter_arr = []
         tour_term_arr = []
         tour_sol_arr = []
         for k in range(51):  # for every point
-            filename = DIMS_MOD +str(k) + "_dim_" + str(dim) + "_cec_" + str(i) + "_tourcand_" + str(j) + "_data"
+            filename = PATH_MOD +str(k) + "_dim_" + str(dim) + "_cec_" + str(i) + "_tourcand_" + str(j) + "_data"
             process_data(filename, iteration_arr=tour_iter_arr, fvalue_arr=tour_fvalue_arr, termination_arr=tour_term_arr,
                          solution_arr=tour_sol_arr)
         cec_fvalue_arr.append(tour_fvalue_arr)
@@ -241,13 +247,13 @@ for i in range(1, 29):  # for every CEC test function 1-28 (i)
     cec_iter_arr = []
     cec_term_arr = []
     cec_sol_arr = []
-    for j in range(2, 7):  # for every size of tournament
+    for j in range(1, 9):  # for every size of tournament
         tour_fvalue_arr = []
         tour_iter_arr = []
         tour_term_arr = []
         tour_sol_arr = []
         for k in range(51):  # for every point
-            filename = DIMS_MOD +str(k) + "_dim_" + str(dim) + "_cec_" + str(i) + "_tourcand_" + str(j) + "_data"
+            filename = PATH_MOD +str(k) + "_dim_" + str(dim) + "_cec_" + str(i) + "_tourcand_" + str(j) + "_data"
             process_data(filename, iteration_arr=tour_iter_arr, fvalue_arr=tour_fvalue_arr, termination_arr=tour_term_arr,
                          solution_arr=tour_sol_arr)
         cec_fvalue_arr.append(tour_fvalue_arr)
@@ -266,18 +272,21 @@ m_dim30_fvalue_arr = []
 m_dim30_termination_arr = []
 m_dim30_solution_arr = []
 m_dim30_iteration_arr = []
+
+
 for i in range(1, 29):  # for every CEC test function 1-28 (i)
     cec_fvalue_arr = []
     cec_iter_arr = []
     cec_term_arr = []
     cec_sol_arr = []
-    for j in range(2, 7):  # for every size of tournament
+    for j in range(1, 9):  # for every size of tournament
         tour_fvalue_arr = []
         tour_iter_arr = []
         tour_term_arr = []
         tour_sol_arr = []
         for k in range(51):  # for every point
-            filename = DIM_30_MOD +str(k) + "_dim_" + str(dim) + "_cec_" + str(i) + "_tourcand_" + str(j) + "_data"
+            filename = PATH_MOD +str(k) + "_dim_" + str(dim) + "_cec_" + str(i) + "_tourcand_" + str(j) + "_data"
+
             process_data(filename, iteration_arr=tour_iter_arr, fvalue_arr=tour_fvalue_arr, termination_arr=tour_term_arr,
                          solution_arr=tour_sol_arr)
         cec_fvalue_arr.append(tour_fvalue_arr)
@@ -290,9 +299,8 @@ for i in range(1, 29):  # for every CEC test function 1-28 (i)
     m_dim30_solution_arr.append(cec_sol_arr)
     m_dim30_iteration_arr.append(cec_iter_arr)
 
-# print(statistics.stdev(m_dim30_fvalue_arr[0][0]))
-# print(dm_string_constructs)
-# dr_sting_constructs = []
+
+
 REF_FVALUES = [dim5_fvalue_arr, dim10_fvalue_arr, dim20_fvalue_arr, dim30_fvalue_arr]
 REF_ITERATIONS = [dim5_iteration_arr, dim10_iteration_arr, dim20_iteration_arr, dim30_iteration_arr]
 REF_TERMINATIONS = [dim5_termination_arr, dim10_termination_arr, dim20_termination_arr, dim30_termination_arr]
@@ -303,86 +311,86 @@ MOD_ITERATIONS = [m_dim5_iteration_arr, m_dim10_iteration_arr, m_dim20_iteration
 MOD_TERMINATIONS = [m_dim5_termination_arr, m_dim10_termination_arr, m_dim20_termination_arr, m_dim30_termination_arr]
 MOD_SOLUTIONS = [m_dim5_solution_arr, m_dim10_solution_arr, m_dim20_solution_arr, m_dim30_solution_arr]
 
+
+
 ### FORMATING PART ###
 
 
-'''
+
 # /min/ /max/ /mean/ /var/ /stdev/ /kwartyl/ /? turniej/
 NUM_OF_COLLUMS = 12
 REF_COLLUMNS = 5
 MOD_COLLUMNS = 6
-i = 0
+'''
+f= open("RESULTS.txt","w+")
+
 OUTPUT = ''
-turniej = 2
+turniej = 1
 #for dim in [5,10,20]:
-for dim in [5,10,20,30]:
-    output_str = '\\begin{table}[H] \n' \
-                 '\\centering \n' \
-                 '\\begin{adjustbox}{width=1\\textwidth}\n' \
-                 '\\small\n' \
-                 '\\begin{tabular}{llllllllllll} \n' \
-                 '\\hline \n' \
-                 '\\multicolumn{' + str(NUM_OF_COLLUMS)+ '}{|c|}{Wymiar = ' + str(dim) + '} \\\\ \n \\hline \n' \
-                                                                                         'F-cja CEC & \\multicolumn{' + str(REF_COLLUMNS)+ '}{|c|}{Oryginalny CMAES} &  \\multicolumn{' + str(MOD_COLLUMNS)+ '}{|c|}{Zmodyfikowany CMAES} \\\\ \n \\hline \n'
-    output_str += ' --- & MIN & MAX & ŚREDNIA & WARIANCJA  & ODCHYLENIE ST  & TURNIEJ & MIN & MAX & ŚREDNIA & WARIANCJA  & ODCHYLENIE ST    \\\\ \n'
-    output_str += '\\hline \n'
-    for cec in range (0,28):
-        # for turn in range (2,7):
-        #     ref_str = ''
-        #     if turn == 2:
-        #         rfv = REF_FVALUES[i][cec]
-        #         multirow = '\\multirow{5}{*}{'
-        #         if rfv != []:
-        #             ref_str = multirow + str(cec) + '} & ' + multirow + str(min(rfv)) + '} & ' +multirow + str(max(rfv)) + '} & ' + multirow + str(statistics.mean(rfv)) + '} & ' + multirow + str(statistics.variance(rfv))+ '} & ' + multirow + str(statistics.stdev(rfv)) + '} & ' + multirow + '} & TEST &'
-        #     mfv = MOD_FVALUES[i][cec][turn-2]
-        #     mod_str = ' & & & & & & \\\\ \n'
-        #     if mfv != []:
-        #         mod_str =str(turn) + ' & ' + str(min(mfv)) + ' & ' + str(max(mfv))+ ' & ' +(str(statistics.mean(mfv))) + ' & ' + str(statistics.variance((mfv))) + ' & ' +  str(statistics.stdev(mfv))+ ' & ' + 'TEST' + '\\\\ \n'
-        #     output_str += (ref_str + mod_str)
-        ref_str = ''
+for turniej in range (6,9):
+    i = 0
+    for dim in [5,10]:
+        output_str = '\\begin{table}[H] \n' \
+                     '\\centering \n' \
+                     '\\begin{adjustbox}{width=1\\textwidth}\n' \
+                     '\\small\n' \
+                     '\\begin{tabular}{llllllllllll} \n' \
+                     '\\hline \n' \
+                     '\\multicolumn{' + str(NUM_OF_COLLUMS)+ '}{|c|}{Wymiar = ' + str(dim) + '} \\\\ \n \\hline \n' \
+                                                                                             'F-cja CEC & \\multicolumn{' + str(REF_COLLUMNS)+ '}{|c|}{Oryginalny CMAES} &  \\multicolumn{' + str(MOD_COLLUMNS)+ '}{|c|}{Zmodyfikowany CMAES} \\\\ \n \\hline \n'
+        output_str += ' --- & MIN & MAX & ŚREDNIA & WARIANCJA  & ODCHYLENIE ST  & TURNIEJ & MIN & MAX & ŚREDNIA & WARIANCJA  & ODCHYLENIE ST    \\\\ \n'
+        output_str += '\\hline \n'
+        for cec in range (0,28):
+            ref_str = ''
 
-        rfv = REF_FVALUES[i][cec]
-        if rfv:
-            ref_str = str(cec+1) + ' & '+  str("%.5g" % min(rfv)) + ' & '+  str( "%.5g" % max(rfv)) + ' & '+  str("%.5g" % statistics.mean(rfv)) + ' & '+  str("%.5g" % statistics.variance(rfv)) + ' & '+ str("%.5g" % statistics.stdev(rfv)) + ' & '
-        mfv = MOD_FVALUES[i][cec][turniej - 2]
-        mod_str = ' & & & & & & \\\\ \n'
-        if mfv:
-            mod_str = str(turniej) + ' & ' + str("%.5g" % min(mfv)) + ' & ' + str("%.5g" % max(mfv))+ ' & ' +  str("%.5g" % statistics.mean(mfv)) + ' & ' + str("%.5g" % statistics.variance((mfv))) + ' & ' +  str("%.5g" % statistics.stdev(mfv)) + '\\\\ \n'
-        output_str += (ref_str + mod_str)
+            rfv = REF_FVALUES[i][cec]
+            if rfv:
+                ref_str = str(cec+1) + ' & '+  str("%.5g" % (min(rfv) - MIN_VALUES[cec])) + ' & '+  str( "%.5g" % (max(rfv) - MIN_VALUES[cec])) + ' & '+  str("%.5g" % (statistics.mean(rfv) - MIN_VALUES[cec])) + ' & '+  str("%.5g" % statistics.variance(rfv)) + ' & '+ str("%.5g" % statistics.stdev(rfv)) + ' & '
+            mfv = MOD_FVALUES[i][cec][turniej - 1]
+            mod_str = ' & & & & & & \\\\ \n'
+            if mfv:
+                mod_str = str(turniej) + ' & ' + str("%.5g" % (min(mfv) - MIN_VALUES[cec])) + ' & ' + str("%.5g" % (max(mfv) - MIN_VALUES[cec]))+ ' & ' +  str("%.5g" % (statistics.mean(mfv) - MIN_VALUES[cec])) + ' & ' + str("%.5g" % statistics.variance(mfv)) + ' & ' +  str("%.5g" % statistics.stdev(mfv)) + '\\\\ \n'
+            output_str += (ref_str + mod_str)
 
-    output_str += '\\hline ' \
-                  '\\end{tabular}\\end{adjustbox}\n' \
-                  '\\end{table}\n'
-    i+=1
-    OUTPUT += output_str
+        output_str += '\\hline ' \
+                      '\\end{tabular}\\end{adjustbox}\n' \
+                      '\\end{table}\n'
+        i+=1
+        OUTPUT += output_str
 print(OUTPUT)
+f.write(OUTPUT)
+
 '''
 
+#'F-cja CEC &  Oryginalny CMAES &  CMAES z turniejem: 2 & CMAES z turniejem: 3 & CMAES z turniejem: 4 & CMAES z turniejem: 5 & CMAES z turniejem: 6 \\\\ \n \\hline \n'
+
+'''
 def most_frequent(List):
     return max(set(List), key = List.count)
 
-NUM_OF_COLLUMS = 7
-OUTPUT = ''
 
-i = 0
-for dim in [5,10,20,30]:
+OUTPUT = ''
+NUM_OF_COLLUMS = 5
+i = 2
+for dim in [20]:
     output_str = '\\begin{table}[H] \n' \
                  '\\centering \n' \
                  '\\begin{adjustbox}{width=1\\textwidth}\n' \
                  '\\small\n' \
-                 '\\begin{tabular}{llllllll} \n' \
+                 '\\begin{tabular}{lllll} \n' \
                  '\\hline \n' \
                  '\\multicolumn{' + str(NUM_OF_COLLUMS)+ '}{|c|}{Wymiar = ' + str(dim) + '} \\\\ \n \\hline \n' \
-                 'F-cja CEC &  Oryginalny CMAES &  CMAES z turniejem: 2 & CMAES z turniejem: 3 & CMAES z turniejem: 4 & CMAES z turniejem: 5 & CMAES z turniejem: 6 \\\\ \n \\hline \n'
+                 'F-cja CEC &  Oryginalny CMAES &  CMAES z porównaniem parami & CMAES z turniejem: 2 & CMAES z turniejem: 3 \\\\ \n \\hline \n'
+
     for cec in range (0,28):
         output_str += str(cec+1) + ' & '
         if REF_TERMINATIONS[i][cec]:
             output_str += most_frequent(REF_TERMINATIONS[i][cec])
         output_str += ' & '
-        for turn in range(0,5):
+        for turn in range(0,3):
             if MOD_TERMINATIONS[i][cec][turn]:
                 output_str += most_frequent(MOD_TERMINATIONS[i][cec][turn])
-            if turn != 4:
+            if turn != 2:
                 output_str += ' & '
             else:
                 output_str += ' \\\\ \n'
@@ -393,5 +401,42 @@ for dim in [5,10,20,30]:
                   '\\end{tabular}\\end{adjustbox}\n' \
                   '\\end{table}\n'
     i+=1
+    OUTPUT += output_str
+print(OUTPUT)
+'''
+
+
+OUTPUT = ''
+NUM_OF_COLLUMS = 5
+i = 2
+for dim in [20, 30]:
+    output_str = '\\begin{table}[H] \n' \
+                 '\\centering \n' \
+                 '\\begin{adjustbox}{width=1\\textwidth}\n' \
+                 '\\small\n' \
+                 '\\begin{tabular}{lllll} \n' \
+                 '\\hline \n' \
+                 '\\multicolumn{' + str(NUM_OF_COLLUMS)+ '}{|c|}{Wymiar = ' + str(dim) + '} \\\\ \n \\hline \n' \
+                 'F-cja CEC &  Oryginalny CMAES &  CMAES z porównaniem parami & CMAES z turniejem: 2 & CMAES z turniejem: 3 \\\\ \n \\hline \n'
+
+    for cec in range (0,28):
+        output_str += str(cec+1) + ' & '
+        if REF_ITERATIONS[i][cec]:
+            output_str += str("%.5g" % statistics.mean(REF_ITERATIONS[i][cec]))
+        output_str += ' & '
+        for turn in range(0,3):
+            if MOD_ITERATIONS[i][cec][turn]:
+                output_str += str("%.5g" % statistics.mean(MOD_ITERATIONS[i][cec][turn]))
+            if turn != 2:
+                output_str += ' & '
+            else:
+                output_str += ' \\\\ \n'
+
+
+
+    output_str += '\\hline ' \
+                  '\\end{tabular}\\end{adjustbox}\n' \
+                  '\\end{table}\n'
+    i += 1
     OUTPUT += output_str
 print(OUTPUT)
